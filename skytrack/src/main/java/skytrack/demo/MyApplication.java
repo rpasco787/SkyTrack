3167109db14d0003c32b4425d1957b33ad4aaeb6
Ryan Pascual <rpascual789@gmail.com>
2026-03-06 21:29:31 -0600

    Add LiveOpenSkyClient with OpenSky state vector parsing

--- skytrack/src/main/java/skytrack/demo/MyApplication.java
+++ skytrack/src/main/java/skytrack/demo/MyApplication.java
@@ -2,9 +2,10 @@ package skytrack.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class MyApplication {
 
 	public static void main(String[] args) {
